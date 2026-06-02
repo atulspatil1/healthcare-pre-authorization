49f287e8f1e2d19d633b517b8fef3e23a1149294
Atul Sanjay Patil <atulspatil82@gmail.com>
2026-06-02 20:55:12 +0530

    intial commit: project skeleton

---
+++ src/main/java/org/atulspatil1/healthcarepreauthorization/HealthcarePreAuthorizationApplication.java
@@ -0,0 +1,13 @@
+package org.atulspatil1.healthcarepreauthorization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthcarePreAuthorizationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HealthcarePreAuthorizationApplication.class, args);
+    }
+
+}
